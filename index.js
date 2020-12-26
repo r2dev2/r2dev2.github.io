5fbd6472512b25acda60f37d6658bba59011525d
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-25 16:14:17 -0800

    Auto-generate bio, pfp

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const username = "r2dev2bb8";
+let gh_data = {};
+
+async function set_avatar() {
+  let pfpImg = document.querySelector("#profilepic");
+  pfpImg.src = gh_data.general.avatar_url;
+}
+
+async function fill_bio() {
+  let bioTag = document.querySelector("#bio");
+  bioTag.textContent = gh_data.general.bio;
+}
+
+async function load_gh_data() {
+  const datas = [
+    {
+      link: `https://api.github.com/users/${username}`,
+      name: "general",
+    }
+  ];
+  await Promise.all(datas.map(async (d) => {
+    let json = await fetch(d.link).then(r => r.json())
+    gh_data[d.name] = json;
+  }));
+}
+
 (async () => {
   let infoSections = document.querySelectorAll("section");
   infoSections.forEach(section => {
@@ -6,4 +32,12 @@
     header.textContent = section.title;
     section.prepend(header);
   });
+
+  await load_gh_data();
+
+  let promises = [
+    fill_bio(),
+    set_avatar(),
+  ];
+  await Promise.all(promises);
 })()
